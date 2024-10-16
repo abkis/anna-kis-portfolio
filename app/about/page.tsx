184d44586ca5ddadfc8e0992a122de8e6423ba96
abkis <kis.anna.borbala@gmail.com>
2024-10-15 21:31:56 -0400

    about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,18 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
 
 export default function Example() {
 	return (
-		<div>
-			<h1>
-				About Me
-			</h1>
+		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+			<Navigation />
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+				<h1 style={{ fontSize: "80%", color: "white" }}>
+					About Me
+				</h1>
+			</div>
 		</div>
 	);
 }

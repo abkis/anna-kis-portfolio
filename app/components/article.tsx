aaa0b93dcf35b412a784d8c771f383d309734cfe
abkis <kis.anna.borbala@gmail.com>
2025-01-14 19:52:38 -0500

    modal for projects

--- app/components/article.tsx
+++ app/components/article.tsx
@@ -1,11 +1,32 @@
+'use client';
 import type { Project } from "../../types/types";
 import Link from "next/link";
 import "../../global.css";
+import { useState } from "react";
+import ProjectModal from "./projectModal";
 
-export const Article: React.FC<{project : Project}> = ({ project }) => {
+export const Article: React.FC<{project : Project, large : boolean}> = ({ project, large }) => {
+	// set up modal functions to open new modal on click
+	const [modalOpen, setModalOpen] = useState(false); // handles open/close of modal
+
+	// open modal on click of image
+	const openModal = (): void =>{
+		setModalOpen(true);
+	}
+	
+	const closeModal = (): void=>{
+		setModalOpen(false);
+	}  
 	return (
-		<Link href={project.slug.startsWith("http") ? project.slug : `/projects/${project.slug}`} target={project.slug == "" ? "_self" : "_blank"}>
-			<article className="p-4 md:p-8">
+		<div>
+		{ modalOpen ? (
+			<ProjectModal closeModal={closeModal} open={openModal} project={project}/>
+			) :
+			(<Link href="#" onClick={(e) => {
+			e.preventDefault(); // Prevents navigation
+			openModal();
+		  }}>
+			<article className={large ? "relative w-full h-full p-4 md:p-8" : "p-4 md:p-8"}>
 				<div className="flex justify-between gap-2 items-center">
 					<span className="text-xs duration-1000 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange" style={{color: "var(--text-color)"}}>
 					{project.start ? (
@@ -29,13 +50,16 @@ export const Article: React.FC<{project : Project}> = ({ project }) => {
                     )}
 					</span>
 				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl group-hover:text-white font-display" style={{color: "var(--text-color)"}}>
+				<h2 className={large ? "mt-4 text-3xl font-bold group-hover:text-white sm:text-4xl font-display" : "z-20 text-xl font-medium duration-1000 lg:text-3xl group-hover:text-white font-display"} 
+					style={{color: "var(--text-color)"}}>
 					{project.name}
 				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 group-hover:text-zinc-200" style={{color: "var(--sub-text-color)"}}>
+				<p className={large ? "mt-4 text-md leading-8 duration-150 group-hover:text-zinc-300" : "z-20 mt-4 text-sm  duration-1000 group-hover:text-zinc-200"}
+					style={{color: "var(--sub-text-color)"}}>
 					{project.content}
 				</p>
 			</article>
-		</Link>
+		</Link>)}
+		</div>
 	);
 };

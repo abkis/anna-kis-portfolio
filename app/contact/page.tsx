a9262136d15855141843563a801f4d5d4e18ff2b
abkis <kis.anna.borbala@gmail.com>
2025-01-17 21:16:50 -0500

    dummy

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
 		<div className="container flex items-center justify-center min-h-screen px-4 mx-auto" >
 			<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16" style={{paddingTop: "5rem"}}>
 				{socials.map((s) => (
-					<Card>
+					<Card dummy={false}>
 						<Link
 							href={s.href}
 							target="_blank"
